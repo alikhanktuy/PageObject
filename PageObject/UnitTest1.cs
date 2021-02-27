035a54491ffd73e6a5d957fec4deb9d7e909b7c5
Нурказган Муслимов <roadtothemiddle@outlook.com>
2021-02-27 17:50:36 +0600

    Learning AQA with C#

---
+++ PageObject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PageObject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
